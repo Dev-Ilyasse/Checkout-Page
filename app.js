0eaee900c88cc4c36a74789a069616039a1daaf6
Dev-Ilyasse <dev.ilyasse@gmail.com>
2021-01-14 02:22:08 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const commande=[
+    {
+        ref:"ref1",
+        prix:"469.99",
+        qte:"4"
+    },
+    {
+        ref:"ref2",
+        prix:"469.99",
+        qte:"3"
+    },
+    {
+        ref:"ref3",
+        prix:"469.99",
+        qte:"5"
+    },
+    {
+        ref:"ref4",
+        prix:"469.99",
+        qte:"1"
+    },
+    {
+        ref:"ref5",
+        prix:"469.99",
+        qte:"6"
+    }
+]
+
